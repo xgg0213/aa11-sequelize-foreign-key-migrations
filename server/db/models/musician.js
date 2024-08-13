a2dbb77c0779aa3eb3cf6e48eacf2e47ca559d26
xgg0213 <m.eringan@gmail.com>
2024-08-13 11:11:09 -0700

    step 1 completed

---
+++ server/db/models/musician.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Musician extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Your code here 
+      Musician.belongsTo(
+        models.Band,
+        { foreignKey: 'id' }
+      )
+    }
+  };
+  Musician.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    bandId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Musician',
+  });
+  return Musician;
+};
